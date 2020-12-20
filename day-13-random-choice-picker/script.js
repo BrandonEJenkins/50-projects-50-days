a8001f3646d657ad7327ea3270e9386a4443a490
BrandonEJenkins <brandonejenkins@gmail.com>
2020-12-19 22:31:24 -0500

    added js to randomly select a tag choice based on a setTimeout function

--- day-13-random-choice-picker/script.js
+++ day-13-random-choice-picker/script.js
@@ -5,13 +5,21 @@ textarea.focus();
 
 textarea.addEventListener('keyup', (event) => {
   createTags(event.target.value);
+
+  if(event.key === 'Enter') {
+    setTimeout(() => {
+      event.target.value = ''
+    }, 10)
+
+    randomSelect();
+  }
 })
 
 function createTags(input) {
-  console.log(input);
+  // console.log(input);
 
   const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
-  console.log(tags);
+  // console.log(tags);
 
   tagsEl.innerHTML = '';
 
@@ -21,4 +29,41 @@ function createTags(input) {
     tagEl.innerText = tag;
     tagsEl.appendChild(tagEl);
   })
+}
+
+function randomSelect() {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100)
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag);
+    }, 100)
+  }, times * 100)
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag'); // querySelectorAll returns a nodelist similar to an array
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+  tag.classList.add('highlight');
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight');
 }
