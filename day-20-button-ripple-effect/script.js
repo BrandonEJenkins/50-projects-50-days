85d21ad9aa08528c408c330d7e0ee0949a1b982e
BrandonEJenkins <brandonejenkins@gmail.com>
2020-12-26 12:13:31 -0500

    added code to calculate position of mouse click and to create span to display on browser

---
+++ day-20-button-ripple-effect/script.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll('.ripple');
+
+buttons.forEach(button => {
+  button.addEventListener('click', function (event) {
+    // get position where click occurs in viewport
+    const x = event.clientX;
+    const y = event.clientY;
+
+    // get position of the button
+    const buttonTop = event.target.offsetTop;
+    const buttonLeft = event.target.offsetLeft;
+
+    //calculate position of the mouse click in the button element
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+
+    // create span with top and left position inline style based on position of mouse click 
+    const circle = document.createElement('span');
+    circle.classList.add('circle');
+    circle.style.top = yInside + 'px';
+    circle.style.left = xInside + 'px';
+
+    this.appendChild(circle);
+
+    setTimeout(() => circle.remove(), 500);
+  })
+})
