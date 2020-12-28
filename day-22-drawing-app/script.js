851dfb038b39cfa8abea10ddaf45bf014ef966dd
BrandonEJenkins <brandonejenkins@gmail.com>
2020-12-28 11:44:12 -0500

    added js to control mouse events

--- day-22-drawing-app/script.js
+++ day-22-drawing-app/script.js
@@ -2,9 +2,44 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let size = 20;
+let isPressed = false; // isPressed is when mouse is down; will togggle using event listener
 let color = 'black';
 let x;
 let y;
+
+canvas.addEventListener('mousedown', (event) => {
+  isPressed = true;
+
+  x = event.offsetX;
+  y = event.offsetY;
+
+  // console.log(isPressed, x, y);
+})
+
+canvas.addEventListener('mousemove', (event) => {
+  if(isPressed) {
+    // store mouse position when mouse button held down and moved
+    const x2 = event.offsetX;
+    const y2 = event.offsetY;
+
+    // console.log(x2, y2);
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
+  }
+})
+
+canvas.addEventListener('mouseup', (event) => {
+  isPressed = false;
+
+  x = undefined;
+  y = undefined;
+
+  // console.log(isPressed, x, y);
+})
 
 function drawCircle(x, y) {
   ctx.beginPath();
@@ -23,6 +58,8 @@ function drawLine(x1, y1, x2, y2) {
 
   // draw a color
   ctx.strokeStyle = color;
-  ctx.lineWidth = size;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
+
+// deal with drag and drop mouse events
