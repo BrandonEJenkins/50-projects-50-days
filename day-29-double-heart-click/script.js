42c413bbfaf92f66aaf7e04be89faabfa13ae5e1
BrandonEJenkins <brandonejenkins@gmail.com>
2021-01-04 17:06:04 -0500

    created customized double click event listener; added function to render heart icon based on double click position in img div

---
+++ day-29-double-heart-click/script.js
@@ -0,0 +1,44 @@
+const loveMe = document.querySelector('.loveMe');
+const times = document.querySelector('#times');
+
+let clickTime = 0;
+let timesClicked = 0;
+
+loveMe.addEventListener('click', (event) => {
+  if(clickTime === 0) {
+    clickTime = new Date().getTime()
+  } else {
+    if((new Date().getTime() - clickTime) < 800) {
+      createHeart(event);
+      clickTime = 0;
+    } else {
+      clickTime = new Date().getTime();
+    }
+  }
+})
+
+const createHeart = (event) => {
+  const heart = document.createElement('i');
+  heart.classList.add('fas');
+  heart.classList.add('fa-heart');
+
+  const x = event.clientX;
+  const y = event.clientY;
+
+  const leftOffset = event.target.offsetLeft;
+  const topOffset = event.target.offsetTop;
+
+  const xInsideImage = x - leftOffset;
+  const yInsideImage = y - topOffset;
+
+  // console.log(xInsideImage, yInsideImage);
+
+  heart.style.top = `${yInsideImage}px`;
+  heart.style.left = `${xInsideImage}px`;
+
+  loveMe.appendChild(heart);
+
+  times.innerHTML = ++timesClicked;
+
+  setTimeout(() => heart.remove(), 2000);
+}
