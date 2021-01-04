e4a49066687b397f46d223b123d12ceace9f5246
BrandonEJenkins <brandonejenkins@gmail.com>
2021-01-04 13:42:24 -0500

    added function to fetch user repos, render element displaying repo name in brower, sorted by recently created repo

--- day-28-github-profiles/script.js
+++ day-28-github-profiles/script.js
@@ -4,18 +4,32 @@ const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
+// get request to get user //
 async function getUser(username) {
   try {
     const { data } = await axios(API_URL + username);
   
     // console.log(data);
     createUserCard(data);
+    getRepos(username);
   } catch(err) {
     if(err.response.status == 404) {
       createErrorCard('No profile with this username');
     }
     // console.log(err);
   }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(API_URL + username + '/repos?sort=created'); // sorted by most recently created repo
+  
+    // console.log(data);
+    addReposToCard(data);
+  } catch(err) {
+      createErrorCard('Problem fetching repos');
+  }
+
 }
 
 function createUserCard(user) {
@@ -51,6 +65,21 @@ function createErrorCard(msg) {
   `
 
   main.innerHTML = cardHTML;
+}
+
+function addReposToCard(repos) {
+  const reposEl = document.getElementById('repos');
+
+  repos
+    .slice(0, 5) // reduces the repos array to the first 5 elements
+    .forEach(repo => {
+      const repoEl = document.createElement('a');
+      repoEl.classList.add('repo');
+      repoEl.href = repo.html_url; // assigns href the value of the repo url from the user/repos response object
+      repoEl.target = '_blank'; // opens in a new window
+      repoEl.innerText = repo.name; // sets text inside link to repo name
+      reposEl.appendChild(repoEl); // inserts each repo element to the dom
+    })
 }
 
 form.addEventListener('submit', (evt) => {
