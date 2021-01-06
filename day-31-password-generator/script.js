9ebe934a4ffd219eac5507d19bb625e135cbc356
BrandonEJenkins <brandonejenkins@gmail.com>
2021-01-06 14:31:08 -0500

    added event listener to button, created functions to generate random symbols based on ascii code

--- day-31-password-generator/script.js
+++ day-31-password-generator/script.js
@@ -1,9 +1,60 @@
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
+
 // create function that assigns a key to each function
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
   number: getRandomNumber,
   symbol: getRandomSymbol
+}
+
+generateEl.addEventListener('click', () => {
+  const length = +lengthEl.value; // the + parses the value in the password length input box to a number
+  // console.log(length);
+
+  // check to see whether boxes are checked; results below will be true or false
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  // console.log(hasLower, hasUpper, hasNumber, hasSymbol);
+  resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+})
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+  
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]); // will filter out anything that has false as a value
+
+  // console.log(typesCount);
+  // console.log(typesArr);
+
+  if(typesCount === 0) {
+    return '';
+  }
+
+  for(let i = 0; i < length; i+= typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type) [0];
+      // console.log(funcName);
+
+      generatedPassword += randomFunc[funcName]()
+    })
+  }
+
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
 }
 
 function getRandomLower() {
