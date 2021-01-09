236e5f673ab051ea4dfb12b5f5943e0d31b754cc
BrandonEJenkins <brandonejenkins@gmail.com>
2021-01-09 00:23:55 -0500

    added images to projects catalog page; added hide images button func to catalog page; updated styles

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const toggle = document.querySelector('.toggle');
+const imgCollapseBtn = document.getElementById('img-collapse');
 
 toggle.addEventListener('click', (event) => {
   const html = document.querySelector('html');
@@ -10,3 +11,28 @@ toggle.addEventListener('click', (event) => {
     event.target.innerHTML = 'Dark Mode'
   }
 })
+
+imgCollapseBtn.addEventListener('click', (e) => toggleCardImage(e));
+
+function toggleCardImage(e) {
+  var x = document.querySelectorAll('.card-img');
+
+  var i;
+
+  for (i = 0; i < x.length; i++) {
+
+    if(x[i].style.display === "none") {
+      x[i].classList.add('show');
+      x[i].classList.remove('hide');
+      x[i].style.display = "block";
+      e.target.innerHTML = 'Hide Images';
+    } else {
+      x[i].classList.add('hide');
+      x[i].classList.remove('show');
+      x[i].style.display = "none";
+      e.target.innerHTML = 'Show Images';
+    }
+
+  }
+
+}
